d686af0a093cdddbe5f2570f88846c53cfc044d5
Osher De Paz <osherdepaz@gmail.com>
2013-11-27 00:36:48 +0200

    Added the "invitations" stage.

---
+++ project/src/com/technion/coolie/letmein/InvitationListActivity.java
@@ -0,0 +1,15 @@
+package com.technion.coolie.letmein;
+
+import android.os.Bundle;
+
+import com.technion.coolie.CoolieActivity;
+import com.technion.coolie.R;
+
+public class InvitationListActivity extends CoolieActivity {
+
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.lmi_activity_invitation_list);
+	}
+
+}
