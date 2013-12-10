b2edd2c4c76d2264f5d3ed584abd218a7cba9126
YanivMD <Yaniv.M.David@gmail.com>
2013-12-11 00:17:52 +0200

    real merge

--- project/src/com/technion/coolie/letmein/InvitationActivity.java
+++ project/src/com/technion/coolie/letmein/InvitationActivity.java
@@ -28,6 +28,7 @@ import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.SimpleAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.TextView.OnEditorActionListener;
 import android.widget.Toast;
@@ -48,7 +49,7 @@ public class InvitationActivity extends DatabaseActivity implements
 	private AutoCompleteTextView friendNameEdit;
 	private EditText friendCellphoneEdit;
 	private EditText friendCarNumberEdit;
-	private AutoCompleteTextView friendCarCompanyEdit;
+	private Spinner friendCarCompanySpinner;
 	private AutoCompleteTextView friendCarColorEdit;
 	private TextView datePicker;
 	private TextView timePicker;
@@ -77,10 +78,8 @@ public class InvitationActivity extends DatabaseActivity implements
 
 		friendCarNumberEdit = (EditText) findViewById(R.id.lmi_friend_car_number_edit);
 
-		friendCarCompanyEdit = (AutoCompleteTextView) findViewById(R.id.lmi_friend_car_company_edit);
-		friendCarCompanyEdit.setAdapter(new ArrayAdapter<String>(
-				InvitationActivity.this, DROP_DOWN_LAYOUT_INDEX, getResources()
-						.getStringArray(R.array.lmi_car_companies)));
+		friendCarCompanySpinner = (Spinner) findViewById(R.id.lmi_friend_car_company_edit);
+
 
 		datePicker = (TextView) findViewById(R.id.lmi_date_picker);
 		setDate(calendar);
@@ -321,9 +320,9 @@ public class InvitationActivity extends DatabaseActivity implements
 		savedInstanceState.putCharSequence(
 				String.valueOf(R.id.lmi_friend_car_number_edit),
 				friendCarNumberEdit.getText());
-		savedInstanceState.putCharSequence(
-				String.valueOf(R.id.lmi_friend_car_company_edit),
-				friendCarCompanyEdit.getText());
+		savedInstanceState.putInt(String.valueOf(R.id.lmi_friend_car_company_edit),
+				friendCarCompanySpinner.getSelectedItemPosition());
+
 		savedInstanceState.putCharSequence(
 				String.valueOf(R.id.lmi_friend_car_color_edit),
 				friendCarColorEdit.getText());
@@ -342,8 +341,10 @@ public class InvitationActivity extends DatabaseActivity implements
 				.valueOf(R.id.lmi_friend_cellphone_edit)));
 		friendCarNumberEdit.setText(savedInstanceState.getCharSequence(String
 				.valueOf(R.id.lmi_friend_car_number_edit)));
-		friendCarCompanyEdit.setText(savedInstanceState.getCharSequence(String
+		friendCarCompanySpinner.setSelection(savedInstanceState.getInt(String
 				.valueOf(R.id.lmi_friend_car_company_edit)));
+
+
 		friendCarColorEdit.setText(savedInstanceState.getCharSequence(String
 				.valueOf(R.id.lmi_friend_car_color_edit)));
 
@@ -429,10 +430,11 @@ public class InvitationActivity extends DatabaseActivity implements
 		final String friendName = friendNameEdit.getText().toString();
 		final String friendCellphone = friendCellphoneEdit.getText().toString();
 		final String carNumber = friendCarNumberEdit.getText().toString();
-		final String carCompany = friendCarCompanyEdit.getText().toString();
+		final CarManufacturer carCompany = CarManufacturer.values()[friendCarCompanySpinner.getSelectedItemPosition()];
 		final String carColor = friendCarColorEdit.getText().toString();
-
-		if (isUserForgotAField(friendName, carNumber, carCompany, carColor))
+	
+		
+		if (isUserForgotAField(friendName, carNumber, carColor))
 			return;
 
 		final Invitation i = Invitation.builder().contactId(friendName)
@@ -447,16 +449,13 @@ public class InvitationActivity extends DatabaseActivity implements
 	}
 
 	private boolean isUserForgotAField(final String friendName,
-			final String carNumber, final String carCompany,
+			final String carNumber,
 			final String carColor) {
 		if ("".equals(friendName))
 			Toast.makeText(getApplicationContext(), "insert name",
 					Toast.LENGTH_SHORT).show();
 		else if ("".equals(carNumber))
 			Toast.makeText(getApplicationContext(), "insert car number",
-					Toast.LENGTH_SHORT).show();
-		else if ("".equals(carCompany))
-			Toast.makeText(getApplicationContext(), "insert car company",
 					Toast.LENGTH_SHORT).show();
 		else if ("".equals(carColor))
 			Toast.makeText(getApplicationContext(), "insert car color",
