5fe36acb4bae3cccf6557e7a174c0e289d17764d
lital amiel <litalamiel@gmail.com>
2013-12-12 00:12:44 +0200

    Added login to facebook

--- project/src/com/technion/coolie/tecmind/GUI/MainActivity.java
+++ project/src/com/technion/coolie/tecmind/GUI/MainActivity.java
@@ -1,5 +1,8 @@
 package com.technion.coolie.tecmind.GUI;
 
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
 import com.technion.coolie.CoolieActivity;
 
 public class MainActivity extends CoolieActivity {
--- project/src/com/technion/coolie/tecmind/GUI/MyTitleActivity.java
+++ project/src/com/technion/coolie/tecmind/GUI/MyTitleActivity.java
@@ -3,12 +3,52 @@ package com.technion.coolie.tecmind.GUI;
 import android.os.Bundle;
 import com.technion.coolie.CoolieActivity;
 import com.technion.coolie.R;
+import com.facebook.*;
+import com.facebook.model.*;
+import android.widget.TextView;
+import android.content.Intent;
 
 public class MyTitleActivity extends CoolieActivity {
-	@Override 
-	protected void onCreate(Bundle savedInstanceState) {
+
+	  @Override
+	  public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.techmind_activity_my_title);
-	}
 
+	    // start Facebook Login
+	    Session.openActiveSession(this, true, new Session.StatusCallback() {
+
+	      // callback when session changes state
+	      @Override
+	      public void call(Session session, SessionState state, Exception exception) {
+	        if (session.isOpened()) {
+
+	          // make request to the /me API
+	          Request.newMeRequest(session, new Request.GraphUserCallback() {
+
+	            // callback after Graph API response with user object
+	            @Override
+	            public void onCompleted(GraphUser user, Response response) {
+//	              if (user != null) {
+//	                TextView welcome = (TextView) findViewById(R.id.welcome);
+//	                welcome.setText("Hello " + user.getName() + "!");
+//	              }
+	            }
+	          }).executeAsync();
+	        }
+	      }
+	    });
+	  }
+
+	  @Override
+	  public void onActivityResult(int requestCode, int resultCode, Intent data) {
+	      super.onActivityResult(requestCode, resultCode, data);
+	      Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
+	  }
 }
+
+
+
+
+
+
