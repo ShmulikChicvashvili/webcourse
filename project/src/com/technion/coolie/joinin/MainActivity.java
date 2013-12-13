5b007aa8b0bf9bff3cf78411f70f34a67fbbb581
Sofia Gengelman <sonyagen@gmail.com>
2013-12-13 22:55:06 +0200

    expandable list adapter view & logic + main activity

--- project/src/com/technion/coolie/joinin/MainActivity.java
+++ project/src/com/technion/coolie/joinin/MainActivity.java
@@ -1,7 +1,82 @@
 package com.technion.coolie.joinin;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import android.os.Bundle;
+import android.widget.ExpandableListView;
+
 import com.technion.coolie.CoolieActivity;
+import com.technion.coolie.R;
+import com.technion.coolie.joinin.data.ClientEvent;
+import com.technion.coolie.joinin.data.EventDate;
+import com.technion.coolie.joinin.gui.ExpandableListAdapter;
+import com.technion.coolie.joinin.map.EventType;
+
+
 
 public class MainActivity extends CoolieActivity {
+	
+ 
+     
+     @Override
+     protected void onCreate(Bundle savedInstanceState) {
+         super.onCreate(savedInstanceState);
+         setContentView(R.layout.ji__expandable_view);
+  
+         // get the list view
+         ExpandableListView expListView = (ExpandableListView) findViewById(R.id.expendable_list);
+  
+         // preparing list data
+         ArrayList<String> listDataHeader = new ArrayList<String>();
+         HashMap<String, List<ClientEvent>> listDataChild = new HashMap<String, List<ClientEvent>>();
+        // DBBridge.prepareListData(listDataHeader,listDataChild);
+         prepareListData(listDataHeader,listDataChild);
+  
+         ExpandableListAdapter listAdapter = new ExpandableListAdapter(this, this , listDataHeader, listDataChild);
+  
+         // setting list adapter
+         expListView.setAdapter(listAdapter);
+     }    
+
+     
+     
+     private void prepareListData(ArrayList<String> listDataHeader,
+ 			HashMap<String, List<ClientEvent>> listDataChild) {
+         
+         // Adding child data
+         listDataHeader.add("I'm Attending");
+         listDataHeader.add("My Events");
+  
+         // Adding child data
+         ArrayList<ClientEvent> attendingArr = new ArrayList<ClientEvent>();
+         attendingArr.add(newEvent("Snooker Match"));
+         attendingArr.add(newEvent("Bear Party"));
+         attendingArr.add(newEvent("Hedva HW Solving"));
+         attendingArr.add(newEvent("BBQ"));
+         attendingArr.add(newEvent("Movie and Bear Night"));
+         
+         
+      // Adding child data
+         ArrayList<ClientEvent> myEventsArr = new ArrayList<ClientEvent>();
+         myEventsArr.add(newEvent("Snooker Match"));
+         myEventsArr.add(newEvent("Bear Party"));
+         myEventsArr.add(newEvent("Hedva HW Solving"));
+         myEventsArr.add(newEvent("BBQ"));
+         myEventsArr.add(newEvent("Movie and Bear Night"));
+
+         listDataChild.put(listDataHeader.get(0), attendingArr); // Header, Child data
+         listDataChild.put(listDataHeader.get(1), myEventsArr); // Header, Child data
+
+     }
+     
+     private ClientEvent newEvent(String name){
+    	 EventDate when = new EventDate();
+    	 final EventType type = EventType.SPORT;
+    	 return new ClientEvent(1, name, "address", "description",
+    			 				20,20, when, 5, type, "owner");
+       }
+
 
 }
---
+++ project/src/com/technion/coolie/joinin/gui/ExpandableListAdapter.java
@@ -0,0 +1,125 @@
+package com.technion.coolie.joinin.gui;
+
+import java.util.HashMap;
+import java.util.List;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.technion.coolie.R;
+import com.technion.coolie.joinin.data.ClientEvent;
+ 
+public class ExpandableListAdapter extends BaseExpandableListAdapter {
+ 
+    private Context _context;
+    private Activity _activity;
+    private List<String> _listDataHeader; // header titles
+    // child data in format of header title, child title
+    private HashMap<String, List<ClientEvent>> _listDataChild;
+ 
+    public ExpandableListAdapter(Context context, Activity act, List<String> listDataHeader,
+            HashMap<String, List<ClientEvent>> listChildData) {
+        _context = context;
+        _listDataHeader = listDataHeader;
+        _listDataChild = listChildData;
+        _activity = act;
+    }
+ 
+    @Override
+    public Object getChild(int groupPosition, int childPosititon) {
+        return this._listDataChild.get(this._listDataHeader.get(groupPosition))
+                .get(childPosititon);
+    }
+ 
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+ 
+    @Override
+    public View getChildView(int groupPosition, final int childPosition,
+            boolean isLastChild, View convertView, ViewGroup parent) {
+ 
+        final ClientEvent eventDetails = (ClientEvent) getChild(groupPosition, childPosition);
+ 
+        if (convertView == null) {
+            LayoutInflater infalInflater = (LayoutInflater) this._context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = infalInflater.inflate(R.layout.ji__event_list_item, null);
+        }
+        
+        setEventView(convertView,eventDetails);
+        
+        return convertView;
+    }
+    
+    private void setEventView(View convertView, ClientEvent eventDetails){
+    
+    	ImageView img = (ImageView)convertView.findViewById(R.id.eventImageView);
+	    TextView name = (TextView)convertView.findViewById(R.id.eventNameText);
+		TextView location = (TextView)convertView.findViewById(R.id.eventLocation);
+		TextView date = (TextView)convertView.findViewById(R.id.eventDate);
+		
+		//img.setImageResource(R.drawable.skel_module2);
+		name.setText(eventDetails.getName());
+		location.setText(eventDetails.getAddress());
+		date.setText(eventDetails.getWhen().toString());
+		Drawable icon = eventDetails.getEventType().getDrawable(_activity);
+		img.setImageDrawable(icon);
+    }
+ 
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return this._listDataChild.get(this._listDataHeader.get(groupPosition))
+                .size();
+    }
+ 
+    @Override
+    public Object getGroup(int groupPosition) {
+        return this._listDataHeader.get(groupPosition);
+    }
+ 
+    @Override
+    public int getGroupCount() {
+        return this._listDataHeader.size();
+    }
+ 
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+ 
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        String headerTitle = (String) getGroup(groupPosition);
+        
+        if (convertView == null) {
+            LayoutInflater infalInflater = (LayoutInflater) this._context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = infalInflater.inflate(R.layout.ji__list_group_string_heade, null);
+        }
+ 
+        TextView lblListHeader = (TextView) convertView.findViewById(R.id.lblListHeader);
+        lblListHeader.setText(headerTitle);
+ 
+        return convertView;
+    }
+ 
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+ 
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return true;
+    }
+}
+
