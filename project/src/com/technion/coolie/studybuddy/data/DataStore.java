31930260e2863b5fe1d5b885fa37035393e31e50
sirotnikov <sirotnikov@gmail.com>
2013-12-15 00:22:42 +0200

    new DataStore

--- project/src/com/technion/coolie/studybuddy/data/DataStore.java
+++ project/src/com/technion/coolie/studybuddy/data/DataStore.java
@@ -3,82 +3,87 @@ package com.technion.coolie.studybuddy.data;
 import java.util.ArrayList;
 import java.util.List;
 
-public class DataStore
-{
+import com.technion.coolie.studybuddy.Models.Course;
+import com.technion.coolie.studybuddy.Models.Semester;
+import com.technion.coolie.studybuddy.Models.StudyResource;
+import com.technion.coolie.studybuddy.PresenterModels.MainPresenterModel;
+
+public class DataStore {
 	private static String[] menus = new String[] { "Tasks", "Courses",
 			"Crazy mode" };
-	public static String[] cources = new String[] { "Data structures",
-			"Operation systems", "Algorithems", "Math" };
-	public static String[] courseNumbers = new String[] { "234218", "234123",
-			"234247", "104014" };
-	private static final List<Task> tasks = new ArrayList<Task>();
-	static
-	{
-		tasks.add(new Task(cources[0], Task.TypeVideo, 1));
-		tasks.add(new Task(cources[0], Task.TypeVideo, 2));
-		tasks.add(new Task(cources[0], Task.TypeLecure, 1));
-		tasks.add(new Task(cources[1], Task.TypeVideo, 1));
-		tasks.add(new Task(cources[1], Task.TypeVideo, 2));
-		tasks.add(new Task(cources[2], Task.TypeVideo, 3));
-	}
+	private static List<Course> courses = new ArrayList<Course>();
+	private static Semester semester = new Semester();
+	private static MainPresenterModel mainPresenterModel;
+
 	public static final int taskForCourse = 14;
 
 	/**
 	 * 
 	 */
-	public DataStore()
-	{
+	public DataStore() {
 		super();
-		// TODO Auto-generated constructor stub
+		addFakeCourses();
 	}
 
-	public static String getCourse(int position)
-	{
-		return cources[position];
+	public static void addFakeCourses() {
+
+		courses.add(new Course(234123, "Operating systems"));
+		courses.add(new Course(234247, "Algorithms"));
+		courses.add(new Course(236353, "Automata and Formal Languages"));
+		courses.add(new Course(134058, "Biology 1"));
+
+		for (Course c : courses) {
+			int i = 4;
+			c.addStudyResources(StudyResource.createWithItems(i--));
+		}
+
 	}
 
-	public static String getMenu(int position)
-	{
-		return menus[position];
+	public static MainPresenterModel getMainPresenterModel() {
+		if (null == mainPresenterModel) {
+			mainPresenterModel = new MainPresenterModel(semester, courses);
+		}
+
+		return mainPresenterModel;
 	}
 
-	public static int getCourcesSize()
-	{
-		return cources.length;
-	}
-
-	public static int getMenuSize()
-	{
-		return menus.length;
-	}
-
-	public static int getTaskSize()
-	{
-		return tasks.size();
-	}
-
-	public static Task getTask(int position)
-	{
-		return tasks.get(position);
-	}
-
-	public static void removeTask(int poistion)
-	{
-		tasks.remove(poistion);
-	}
-
-	public static void removeTask(String item)
-	{
-		tasks.remove(item);
-	}
-
-	public static void addTask(Task task)
-	{
-		tasks.add(task);
-	}
-
-	public static String getCourseNumber(int position)
-	{
-		return courseNumbers[position];
-	}
+	// public public static Course getCourse(int position) {
+	// return courses.get(position);
+	// }
+	//
+	// public static String getMenu(int position) {
+	// return menus[position];
+	// }
+	//
+	// public static int getCoursesCount() {
+	// return courses.size();
+	// }
+	//
+	// public static int getMenuSize() {
+	// return menus.length;
+	// }
+	//
+	// public static int getTaskSize() {
+	// return tasks.size();
+	// }
+	//
+	// public static Task getTask(int position) {
+	// return tasks.get(position);
+	// }
+	//
+	// public static void removeTask(int poistion) {
+	// tasks.remove(poistion);
+	// }
+	//
+	// public static void removeTask(String item) {
+	// tasks.remove(item);
+	// }
+	//
+	// public static void addTask(Task task) {
+	// tasks.add(task);
+	// }
+	//
+	// public static String getCourseNumber(int position) {
+	// return courseNumbers[position];
+	// }
 }
