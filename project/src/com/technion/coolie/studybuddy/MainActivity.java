c569c1db61ad2861718ef5faeeabaf8ff9a1e7d3
sirotnikov <sirotnikov@gmail.com>
2013-12-15 06:29:17 +0200

    typo

--- project/src/com/technion/coolie/studybuddy/MainActivity.java
+++ project/src/com/technion/coolie/studybuddy/MainActivity.java
@@ -1,6 +1,5 @@
 package com.technion.coolie.studybuddy;
 
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -11,10 +10,9 @@ import com.technion.coolie.R;
 import com.technion.coolie.studybuddy.Adapters.CourseAdapter;
 import com.technion.coolie.studybuddy.Views.NowLayout;
 import com.technion.coolie.studybuddy.Views.StudyBuddyActivity;
-import com.technion.coolie.studybuddy.Views.TasksActicity;
+import com.technion.coolie.studybuddy.Views.TasksActivity;
 
-public class MainActivity extends StudyBuddyActivity
-{
+public class MainActivity extends StudyBuddyActivity {
 
 	/*
 	 * (non-Javadoc)
@@ -22,24 +20,23 @@ public class MainActivity extends StudyBuddyActivity
 	 * @see com.technion.coolie.CoolieActivity#onCreate(android.os.Bundle)
 	 */
 	@Override
-	protected void onCreate(Bundle savedInstanceState)
-	{
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.stb_view_main);
-		
+
+		// DataStore.addFakeCourses();
+
 		ImageView imageView = (ImageView) findViewById(R.id.graph_view);
-		imageView.setOnClickListener(new OnClickListener()
-		{
+		imageView.setOnClickListener(new OnClickListener() {
 
 			@Override
-			public void onClick(View v)
-			{
+			public void onClick(View v) {
 				v.getContext().startActivity(
-						new Intent(v.getContext(), TasksActicity.class));
+						new Intent(v.getContext(), TasksActivity.class));
 			}
 		});
 		NowLayout layout = (NowLayout) findViewById(R.id.course_list);
-	
+
 		CourseAdapter adapter = new CourseAdapter(this);
 		layout.setAdapter(adapter);
 	}
