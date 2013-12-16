96c0896814a5c65315b277575f787ef9060aadbf
alex nicola <alexnicola07@gmail.com>
2013-12-16 21:36:28 +0200

    Added Server Interface

--- project/src/com/technion/coolie/CoolieServerInterface.java
+++ project/src/com/technion/coolie/CoolieServerInterface.java
@@ -2,6 +2,12 @@ package com.technion.coolie;
 
 
 
+import java.util.List;
+
+import com.google.gson.Gson;
+import com.technion.coolie.server.ug.framework.CourseKey;
+import com.technion.coolie.server.ug.framework.Semester;
+import com.technion.coolie.server.ug.framework.Student;
 import com.technion.coolie.skeleton.CoolieServerInterfaceService;
 import com.technion.coolie.skeleton.CoolieStatus;
 
@@ -11,13 +17,33 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 
+
+/*this is how u will use it on a your activity, u will need to create CoolieServerInterface for each function u do,
+ * and deserialize the Gson appropriately on each handleResult u write
+ * CoolieServerInterface c = new CoolieServerInterface(getApplicationContext()){
+
+					@Override
+					public void handleResult(String result, CoolieStatus status) {
+						new Gson().fromJson(result, new TypeToken<List<CourseServer>>() {
+					    }.getType());
+						
+					}
+				};
+				Semester semester = new Semester();
+				c.getAllCourses(semester);
+ */
+
+
 public abstract class CoolieServerInterface {
 	Context mContext;
 	
-	enum Action
+	public enum Action
 	{
+		UG_GET_ALL_COURSES,
+		UG_GET_COURSES,
+		UG_GET_STUDENT_CURRENT_COURSES,
 		
-		
+	
 	}
 	public CoolieServerInterface(Context c)
 	{
@@ -51,14 +77,29 @@ public abstract class CoolieServerInterface {
 	}
 	
 	
-	public void doActionOnServer(Action action,String... input) {
+	public void getAllCourses(Semester semester) {
 		Intent intent = new Intent(mContext, CoolieServerInterfaceService.class);
-		// add infos for the service which file to download and where to store
-		intent.putExtra(CoolieServerInterfaceService.INPUT,input);			
+		Action action = Action.UG_GET_ALL_COURSES;
+		intent.putExtra(CoolieServerInterfaceService.ACTION,action);
+		intent.putExtra(CoolieServerInterfaceService.RESULT,semester);		
 		mContext.startService(intent);
 	}
 	
+	public void getCourses(List<CourseKey> courseKeys) {
+		Intent intent = new Intent(mContext, CoolieServerInterfaceService.class);
+		Action action = Action.UG_GET_COURSES;
+		intent.putExtra(CoolieServerInterfaceService.ACTION,action);
+		intent.putExtra(CoolieServerInterfaceService.RESULT, new Gson().toJson(courseKeys));
+		mContext.startService(intent);
+	}
 	
+	public void getStudentCurrentCourses(Student student) {
+		Intent intent = new Intent(mContext, CoolieServerInterfaceService.class);
+		Action action = Action.UG_GET_STUDENT_CURRENT_COURSES;
+		intent.putExtra(CoolieServerInterfaceService.ACTION,action);
+		intent.putExtra(CoolieServerInterfaceService.RESULT, new Gson().toJson(student));
+		mContext.startService(intent);
+	}
 }
 
 
--- project/src/com/technion/coolie/skeleton/CoolieServerInterfaceService.java
+++ project/src/com/technion/coolie/skeleton/CoolieServerInterfaceService.java
@@ -1,14 +1,33 @@
 package com.technion.coolie.skeleton;
 
+import java.util.List;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.technion.coolie.CoolieServerInterface;
+import com.technion.coolie.CoolieServerInterface.Action;
+import com.technion.coolie.server.ug.api.UgFactory;
+import com.technion.coolie.server.ug.framework.CourseKey;
+import com.technion.coolie.server.ug.framework.CourseServer;
+import com.technion.coolie.server.ug.framework.Semester;
+import com.technion.coolie.server.ug.framework.Student;
+
 import android.app.IntentService;
 import android.content.Intent;
+
 
 public class CoolieServerInterfaceService extends IntentService {
 
 	public static final String INPUT = "input";
+	public static final String ACTION = "action";
 	public static final String STATUS = "status";
 	public static final String RESULT = "result";
+	
+	
 	public static final String NOTIFICATION = "com.technion.coolie.CoolieServerInterface";
+	
+	
+	public static final String SEMESTER = "semester";
 	
 	
 	public CoolieServerInterfaceService() {
@@ -19,9 +38,58 @@ public class CoolieServerInterfaceService extends IntentService {
 	@Override
 	protected void onHandleIntent(Intent arg0) {
 		Intent intent = new Intent(NOTIFICATION);
+		CoolieServerInterface.Action action = (Action) arg0.getSerializableExtra(ACTION);
+		
+		switch (action)
+		{
+			case UG_GET_ALL_COURSES:
+			{
+				Semester semester = (Semester) arg0.getSerializableExtra(RESULT);
+				List<CourseServer> list;
+				list = UgFactory.getUgCourse().getAllCourses(semester);
+				
+				intent.putExtra(STATUS, CoolieStatus.RESULT_OK);
+				intent.putExtra(CoolieServerInterfaceService.RESULT, new Gson().toJson(list));
+				sendBroadcast(intent);
+				break;
+			}
+		
+			case UG_GET_COURSES:
+			{
+				List<CourseKey> listInput;
+				List<CourseServer> listOutput;
+				listInput = new Gson().fromJson((String)arg0.getSerializableExtra(RESULT), new TypeToken<List<CourseKey>>() {
+			    }.getType());
+				
+				listOutput = UgFactory.getUgCourse().getCourses(listInput);
+				intent.putExtra(STATUS, CoolieStatus.RESULT_OK);
+				intent.putExtra(CoolieServerInterfaceService.RESULT, new Gson().toJson(listOutput));
+				sendBroadcast(intent);
+				break;
+			}
+			
+			case UG_GET_STUDENT_CURRENT_COURSES:
+			{
+				Student student;
+				List<CourseServer> listOutput;
+				student = new Gson().fromJson((String)arg0.getSerializableExtra(RESULT),Student.class);
+				
+				listOutput = UgFactory.getUgCourse().getStudentCurrentCourses(student);
+				intent.putExtra(STATUS, CoolieStatus.RESULT_OK);
+				intent.putExtra(CoolieServerInterfaceService.RESULT, new Gson().toJson(listOutput));
+				sendBroadcast(intent);
+				break;
+			}
+				
 		
 		
-		sendBroadcast(intent);
+		
+		
+		
+		
+		
+		}
+		
 	}
 
 }
