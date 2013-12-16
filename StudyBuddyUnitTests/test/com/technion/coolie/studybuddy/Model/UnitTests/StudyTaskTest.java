b488d0c5de1b92d38754a8a9e3d803f44eff6b8b
sirotnikov <sirotnikov@gmail.com>
2013-12-16 14:31:06 +0200

    package rename

--- StudyBuddyUnitTests/test/com/technion/coolie/studybuddy/Model/UnitTests/StudyTaskTest.java
+++ StudyBuddyUnitTests/test/com/technion/coolie/studybuddy/Model/UnitTests/StudyTaskTest.java
@@ -10,7 +10,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.technion.coolie.studybuddy.Models.StudyItem;
+import com.technion.coolie.studybuddy.models.StudyItem;
 
 public class StudyTaskTest {
 
