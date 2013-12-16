b5a5a421acd165c9d8e81bba9d4d92cb9732c284
sirotnikov <sirotnikov@gmail.com>
2013-12-16 14:37:52 +0200

    semester data in semester class

--- project/src/com/technion/coolie/studybuddy/models/Semester.java
+++ project/src/com/technion/coolie/studybuddy/models/Semester.java
@@ -1,5 +1,51 @@
 package com.technion.coolie.studybuddy.models;
 
-public class Semester {
+import java.util.Date;
 
+import com.j256.ormlite.table.DatabaseTable;
+
+@DatabaseTable(tableName = "semesters")
+public class Semester
+{
+
+	public final static int		WEEKS_IN_SEMESTER	= 14;
+	private Date				startDate;
+	private Date				endDate;
+	private final static long	MILISECONDS_IN_WEEK	= 1000 * 60 * 60 * 24 * 7;
+
+	public Semester()
+	{
+
+	}
+
+	public Date getStartDate()
+	{
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate)
+	{
+		this.startDate = startDate;
+	}
+
+	public Date getEndDate()
+	{
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate)
+	{
+		this.endDate = endDate;
+	}
+
+	public int getSemesterWeek(Date today)
+	{
+		return Semester.countWeeksBetween(startDate, today) + 1;
+	}
+
+	public static int countWeeksBetween(Date start, Date end)
+	{
+		int result = (int) ((end.getTime() - start.getTime()) / MILISECONDS_IN_WEEK);
+		return result;
+	}
 }
