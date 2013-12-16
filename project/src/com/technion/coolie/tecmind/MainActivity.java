393feb2b2695aa9367bc21f89933d7f898605ff3
lital amiel <litalamiel@gmail.com>
2013-12-16 22:14:48 +0200

    deleted myTitleActivity

--- project/src/com/technion/coolie/tecmind/MainActivity.java
+++ project/src/com/technion/coolie/tecmind/MainActivity.java
@@ -1,21 +1,53 @@
 package com.technion.coolie.tecmind;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.facebook.Request;
+import com.facebook.Response;
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.model.GraphUser;
 import com.technion.coolie.CoolieActivity;
 import com.technion.coolie.R;
 
 public class MainActivity extends CoolieActivity {
-
-	
 	  @Override
 	  public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.techmind_activity_my_title);
-	  }
-	  
-}
 
+	    // start Facebook Login
+	    Session.openActiveSession(this, true, new Session.StatusCallback() {
+
+	      // callback when session changes state
+	      @Override
+	      public void call(Session session, SessionState state, Exception exception) {
+	        if (session.isOpened()) {
+
+	          // make request to the /me API
+	          Request.newMeRequest(session, new Request.GraphUserCallback() {
+
+	            // callback after Graph API response with user object
+	            @Override
+	            public void onCompleted(GraphUser user, Response response) {
+//	              if (user != null) {
+//	                TextView welcome = (TextView) findViewById(R.id.welcome);
+//	                welcome.setText("Hello " + user.getName() + "!");
+//	              }
+	            }
+	          }).executeAsync();
+	        }
+	      }
+	    });
+	  }
+
+	  @Override
+	  public void onActivityResult(int requestCode, int resultCode, Intent data) {
+	      super.onActivityResult(requestCode, resultCode, data);
+	      Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
+	  }
+}
 
 
 
