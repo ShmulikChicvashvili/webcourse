4b437b2db11868086500918991fae98a0759b599
sirotnikov <sirotnikov@gmail.com>
2013-12-16 20:29:50 +0200

    t

--- project/src/com/technion/coolie/studybuddy/MainActivity.java
+++ project/src/com/technion/coolie/studybuddy/MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.LinearLayout;
 
 import com.actionbarsherlock.view.Menu;
 import com.technion.coolie.R;
-import com.technion.coolie.StbSettingsActivity;
+//import com.technion.coolie.StbSettingsActivity;
 import com.technion.coolie.studybuddy.adapters.CourseAdapter;
 import com.technion.coolie.studybuddy.data.DataStore;
 import com.technion.coolie.studybuddy.graphs.GraphFactory;
@@ -64,7 +64,7 @@ public class MainActivity extends StudyBuddyActivity
 			startActivity(intent);
 			return true;
 		case R.id.stb_main_settings:
-			intent = new Intent(this, StbSettingsActivity.class);
+			// intent = new Intent(this, StbSettingsActivity.class);
 			startActivity(intent);
 			return true;
 		}
