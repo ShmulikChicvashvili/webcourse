912728d0f7189a46661f2e5db3550ad2d6cc415c
sirotnikov <sirotnikov@gmail.com>
2013-12-16 14:37:36 +0200

    courses can return id as string. renamed functions in course

--- project/src/com/technion/coolie/studybuddy/models/Course.java
+++ project/src/com/technion/coolie/studybuddy/models/Course.java
@@ -9,97 +9,119 @@ import java.util.Set;
 
 import com.technion.coolie.studybuddy.utils.Utils;
 
-public class Course implements Comparable<Course> {
+public class Course implements Comparable<Course>
+{
 
-	private final List<Exam> exams = new ArrayList<Exam>();
-	private final Set<StudyResource> trackedResouces = new HashSet<StudyResource>();
-	private final int id;
-	private final String name;
+	private final List<Exam>			exams			= new ArrayList<Exam>();
+	private final Set<StudyResource>	trackedResouces	= new HashSet<StudyResource>();
+	private final int					id;
+	private final String				name;
 
-	public Course() {
+	public Course()
+	{
 		id = radomId();
 		name = String.valueOf(id);
 	}
 
-	private static int radomId() {
+	private static int radomId()
+	{
 		return Utils.randomInt(999999);
 
 	}
 
-	public Course(int id, String name) {
+	public Course(int id, String name)
+	{
 		this.id = id;
 		this.name = name;
 	}
 
-	public List<Exam> getExams() {
+	public List<Exam> getExams()
+	{
 		return exams;
 	}
 
-	public void addExam(Exam e) {
+	public void addExam(Exam e)
+	{
 		exams.add(e);
 	}
 
-	public void addExams(Exam... exams) {
+	public void addExams(Exam... exams)
+	{
 		this.exams.addAll(Arrays.asList(exams));
 	}
 
-	public void addExams(Collection<Exam> exams) {
+	public void addExams(Collection<Exam> exams)
+	{
 		this.exams.addAll(exams);
 	}
 
-	public String getName() {
+	public String getName()
+	{
 		return name;
 	}
 
-	public int getId() {
+	public int getId()
+	{
 		return id;
 	}
 
-	public Set<StudyResource> getStudyResources() {
+	public Set<StudyResource> getStudyResources()
+	{
 		return trackedResouces;
 	}
 
-	public void addStudyResource(StudyResource r) {
+	public void addStudyResource(StudyResource r)
+	{
 		trackedResouces.add(r);
 	}
 
-	public void addStudyResources(StudyResource... r) {
+	public void addStudyResources(StudyResource... r)
+	{
 		trackedResouces.addAll(Arrays.asList(r));
 	}
 
-	public void addStudyResources(Collection<StudyResource> r) {
+	public void addStudyResources(Collection<StudyResource> r)
+	{
 		trackedResouces.addAll(r);
 	}
 
-	public int getStudyItemsTotal() {
+	public int getStudyItemsTotal()
+	{
 		int sum = 0;
-		for (StudyResource sr : trackedResouces) {
-			sum += sr.getItemsTotal();
+		for (StudyResource sr : trackedResouces)
+		{
+			sum += sr.getTotalItemCount();
 		}
 		return sum;
 	}
 
-	public List<StudyItem> getStudyItems() {
+	public List<StudyItem> getStudyItems()
+	{
 		List<StudyItem> list = new ArrayList<StudyItem>();
-		for (StudyResource sr : trackedResouces) {
+		for (StudyResource sr : trackedResouces)
+		{
 			list.addAll(sr.getAllItems());
 		}
 		return list;
 	}
 
-	public int getNumStudyItemsRemaining() {
+	public int getNumStudyItemsRemaining()
+	{
 		int total = 0;
-		for (StudyResource sr : trackedResouces) {
-			total += sr.getNumItemsRemaining();
+		for (StudyResource sr : trackedResouces)
+		{
+			total += sr.getRemainingItemsCount();
 		}
 		return total;
 
 	}
 
-	public List<StudyItem> getStudyItemsRemaining() {
+	public List<StudyItem> getStudyItemsRemaining()
+	{
 
 		List<StudyItem> list = new ArrayList<StudyItem>();
-		for (StudyResource sr : trackedResouces) {
+		for (StudyResource sr : trackedResouces)
+		{
 			list.addAll(sr.getItemsRemaining());
 		}
 		return list;
@@ -107,13 +129,38 @@ public class Course implements Comparable<Course> {
 	}
 
 	@Override
-	public int compareTo(Course another) {
+	public int compareTo(Course another)
+	{
 		return another.getNumStudyItemsRemaining()
 				- getNumStudyItemsRemaining();
 	}
 
 	@Override
-	public String toString() {
+	public String toString()
+	{
 		return String.valueOf(id) + " " + name;
+	}
+
+	public String getIdAsString()
+	{
+		return String.valueOf(id);
+	}
+
+	public int getResourceTotalItemCount(String name)
+	{
+		return getResourceByName(name);
+	}
+
+	private int getResourceByName(String name)
+	{
+		for (StudyResource sr : trackedResouces)
+		{
+			if (sr.getName() == name)
+			{
+				sr.getTotalItemCount();
+			}
+		}
+
+		return 0;
 	}
 }
