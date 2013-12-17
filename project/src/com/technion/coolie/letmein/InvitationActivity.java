45933e63b1d9c4f0b2f270712bb3460808932ccb
YanivMD <Yaniv.M.David@gmail.com>
2013-12-17 22:34:24 +0200

    fixed toast crash

--- project/src/com/technion/coolie/letmein/InvitationActivity.java
+++ project/src/com/technion/coolie/letmein/InvitationActivity.java
@@ -330,24 +330,14 @@ public class InvitationActivity extends DatabaseActivity implements CalendarSupp
 							(String) params[0], (String) params[1], (Invitation) params[2]));
 				} catch (final ConnectionException e) {
 					$ = new AsyncTaskResult<Boolean>(e);
-					Toast.makeText(InvitationActivity.this, "There was a connection problem",
-							Toast.LENGTH_LONG).show();
 				} catch (final UnknownErrorException e) {
 					$ = new AsyncTaskResult<Boolean>(e);
-					Toast.makeText(InvitationActivity.this, "There was a unknown problem",
-							Toast.LENGTH_LONG).show();
 				} catch (final LoginException e) {
 					$ = new AsyncTaskResult<Boolean>(e);
-					Toast.makeText(InvitationActivity.this, "There was a login problem",
-							Toast.LENGTH_LONG).show();
 				} catch (final InvitationFormatException e) {
 					$ = new AsyncTaskResult<Boolean>(e);
-					Toast.makeText(InvitationActivity.this, "There was a format problem",
-							Toast.LENGTH_LONG).show();
 				} catch (final InvitationLimitExceededException e) {
 					$ = new AsyncTaskResult<Boolean>(e);
-					Toast.makeText(InvitationActivity.this, "There was a Limit problem",
-							Toast.LENGTH_LONG).show();
 				}
 
 				return $;
@@ -355,9 +345,34 @@ public class InvitationActivity extends DatabaseActivity implements CalendarSupp
 
 			@Override
 			protected void onPostExecute(final AsyncTaskResult<Boolean> result) {
-				if (result.getError() == null)
-					// TODO
+				final Exception e = result.getError();
+				if (e == null)
 					return;
+				if (e instanceof ConnectionException) {
+					Toast.makeText(InvitationActivity.this, "There was a connection problem",
+							Toast.LENGTH_LONG).show();
+					return;
+				}
+				if (e instanceof UnknownErrorException) {
+					Toast.makeText(InvitationActivity.this, "There was a unknown problem",
+							Toast.LENGTH_LONG).show();
+					return;
+				}
+				if (e instanceof LoginException) {
+					Toast.makeText(InvitationActivity.this, "There was a login problem",
+							Toast.LENGTH_LONG).show();
+					return;
+				}
+				if (e instanceof InvitationFormatException) {
+
+					Toast.makeText(InvitationActivity.this, "There was a format problem",
+							Toast.LENGTH_LONG).show();
+					return;
+				}
+
+				if (e instanceof InvitationLimitExceededException)
+					Toast.makeText(InvitationActivity.this, "There was a Limit problem",
+							Toast.LENGTH_LONG).show();
 			}
 
 		}).execute(username, password, i);
