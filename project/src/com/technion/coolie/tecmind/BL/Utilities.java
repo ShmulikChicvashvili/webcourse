413d666650f218b2032aa2ddfc636fff97f10cfc
lital amiel <litalamiel@gmail.com>
2013-12-17 21:41:34 +0200

    Integrate between layouts and Logic

--- project/src/com/technion/coolie/tecmind/BL/Utilities.java
+++ project/src/com/technion/coolie/tecmind/BL/Utilities.java
@@ -4,45 +4,55 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-
-
 public class Utilities {
 	public class LikesObject {
 		String id;
 		String name;
 	}
-	
+
 	public class CommentObject {
 		boolean user_likes;
 		String message;
 		String id;
 		boolean can_remove;
-//		ArrayList<UserObject> from;
+		// ArrayList<UserObject> from;
 		String created_time;
 		int like_count;
 	}
-	
+
 	public class UserObject {
 		String id;
 		String name;
 	}
-	
-	public static void calculatePosts(int numOfPosts){
-		User checkUser = User.getUserInstance(null);
-		Post temp = new Post(null, null, null, 0, 0);
+
+	public static void calculatePosts(int numOfPosts) {
 		User.getUserInstance(null).postsNum += numOfPosts;
-		User.getUserInstance(null).totalTechoins += (temp.technionValue)*numOfPosts;
-		
+		User.getUserInstance(null).totalTechoins += calculatePostsAux(numOfPosts);
 	}
-	
-	public static void calculateComments(Post post, int numOfComments){
-		
+
+	public static int calculatePostsAux(int numOfPosts) {
+		Post temp = new Post(null, null, null, 0, 0);
+		return (temp.technionValue) * numOfPosts;
+	}
+
+	public static int calculateCommentsAux(int numOfComments) {
+		Comment temp = new Comment(null, null, 0, 0);
+		return (temp.technionValue) * numOfComments;
+	}
+
+	public static int calculatelikesAux(int numOfLikes) {
+		Like temp = new Like(null, null, 0);
+		return (temp.technionValue) * numOfLikes;
+	}
+
+	public static void calculateComments(Post post, int numOfComments) {
+
 		if (post != null) {
 			if (post.commentCount < numOfComments) {
 				User checkUser = User.getUserInstance(null);
 				// dummy comment to get the value of one comment
 				Comment temp = new Comment(null, null, 0, 0);
-				
+
 				// calculates how many comments were collected
 				int diff = numOfComments - post.commentCount;
 				// updates the commentCount of the post
@@ -50,19 +60,20 @@ public class Utilities {
 				// updates the commentNum of the user
 				User.getUserInstance(null).commentsNum += diff;
 				// updates the amount of Techoins of the user
-				User.getUserInstance(null).totalTechoins += diff*temp.technionValue;
+				User.getUserInstance(null).totalTechoins += diff
+						* temp.technionValue;
 			}
 		}
-	}	
-	
-	public static void calculateLikes(Post post, int numOfLikes){
-		
+	}
+
+	public static void calculateLikes(Post post, int numOfLikes) {
+
 		if (post != null) {
 			if (post.likesCount < numOfLikes) {
 				User checkUser = User.getUserInstance(null);
 				// dummy like to get the value of one like
 				Like temp = new Like(null, null, 0);
-				
+
 				// calculates how many likes were collected
 				int diff = numOfLikes - post.likesCount;
 				// updates the likesCount of the post
@@ -70,22 +81,22 @@ public class Utilities {
 				// updates the likestNum of the user
 				User.getUserInstance(null).likesOnPostsNum += diff;
 				// updates the amount of Techoins of the user
-				User.getUserInstance(null).totalTechoins += diff*temp.technionValue;
+				User.getUserInstance(null).totalTechoins += diff
+						* temp.technionValue;
 			}
 		}
 	}
-	
 
+	public static Date parseDate(String str) {
+		SimpleDateFormat sdf = new SimpleDateFormat(
+				"yyyy-MM-dd'T'HH:mm:ss+SSSS");
+		Date date = new Date();
+		try {
+			date = sdf.parse(str);
+		} catch (ParseException e) {
+			System.out.println("Unable to parse date string");
+		}
 
-    public static Date parseDate(String str) {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss+SSSS");
-        Date date = new Date();
-        try {
-            date = sdf.parse(str);
-        } catch(ParseException e) {
-            System.out.println("Unable to parse date string");
-        }
-        
 		return date;
-      }
+	}
 }
--- project/src/com/technion/coolie/tecmind/TotalAccountActivity.java
+++ project/src/com/technion/coolie/tecmind/TotalAccountActivity.java
@@ -1,19 +1,47 @@
 package com.technion.coolie.tecmind;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.actionbarsherlock.app.SherlockFragment;
+import com.facebook.HttpMethod;
+import com.facebook.Request;
+import com.facebook.Response;
+import com.facebook.Session;
+import com.facebook.model.GraphObject;
 import com.technion.coolie.R;
+import com.technion.coolie.tecmind.BL.Mine;
+import com.technion.coolie.tecmind.BL.Post;
+import com.technion.coolie.tecmind.BL.User;
+import com.technion.coolie.tecmind.MineActivity.ServerUpdateUserData;
+import com.technion.coolie.tecmind.server.ReturnCode;
+import com.technion.coolie.tecmind.server.TecPost;
+import com.technion.coolie.tecmind.server.TecUser;
+import com.technion.coolie.tecmind.server.TecUserTitle;
+import com.technion.coolie.tecmind.server.TechmineAPI;
 
 public class TotalAccountActivity extends SherlockFragment {
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
-        return (LinearLayout) inflater.inflate(R.layout.techmind_activity_total_account, container, false);
 
-    }
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View inflateView = (LinearLayout) inflater.inflate(
+				R.layout.techmind_activity_total_account, container, false);
+		TextView from = (TextView) inflateView.findViewById(R.id.from_data);
+		from.setText(MineActivity.exMiningDate.toString());
+		TextView to = (TextView) inflateView.findViewById(R.id.to_data);
+		to.setText(MineActivity.newMiningDate.toString());
+		TextView total = (TextView) inflateView.findViewById(R.id.total_data);
+		total.setText(String.valueOf(MineActivity.totalDelta));
+		return inflateView;
+	}
 }
