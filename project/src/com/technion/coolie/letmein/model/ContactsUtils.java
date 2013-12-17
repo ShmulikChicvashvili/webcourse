7e064eb5d76776b7b3d819876fcab66780124cb2
Gilad Maymon <sgmay@t2.technion.ac.il>
2013-12-17 21:05:45 +0200

    Sent ArrivalTime twice instead of ArrivalDate, ArrivalTime

--- project/src/com/technion/coolie/letmein/model/ContactsUtils.java
+++ project/src/com/technion/coolie/letmein/model/ContactsUtils.java
@@ -22,76 +22,94 @@ public class ContactsUtils {
 		if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.HONEYCOMB)
 			projection.add(ContactsContract.CommonDataKinds.Phone.PHOTO_THUMBNAIL_URI);
 
-		final Cursor nameCur = contentResolver.query(ContactsContract.Data.CONTENT_URI,
-				projection.toArray(new String[projection.size()]), ContactsContract.Data.MIMETYPE
-						+ " = ?",
-				new String[] { ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE },
-				ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME);
+		Cursor nameCursor = null;
+		Cursor phoneCursor = null;
+		Cursor emailCursor = null;
+		try {
+			nameCursor = contentResolver
+					.query(ContactsContract.Data.CONTENT_URI,
+							projection.toArray(new String[projection.size()]),
+							ContactsContract.Data.MIMETYPE + " = ?",
+							new String[] { ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE },
+							ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME);
 
-		while (nameCur.moveToNext()) {
-			final String name = nameCur.getString(nameCur
-					.getColumnIndex(ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME));
+			while (nameCursor.moveToNext()) {
+				final String name = nameCursor
+						.getString(nameCursor
+								.getColumnIndex(ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME));
 
-			final Long id = nameCur.getLong(nameCur
-					.getColumnIndex(ContactsContract.CommonDataKinds.StructuredName.CONTACT_ID));
-			final String idStr = String.valueOf(id);
+				final Long id = nameCursor
+						.getLong(nameCursor
+								.getColumnIndex(ContactsContract.CommonDataKinds.StructuredName.CONTACT_ID));
+				final String idStr = String.valueOf(id);
 
-			String imageUri;
-			if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.HONEYCOMB)
-				imageUri = nameCur
-						.getString(nameCur
-								.getColumnIndex(ContactsContract.CommonDataKinds.Phone.PHOTO_THUMBNAIL_URI));
-			else
-				imageUri = null;
+				String imageUri;
+				if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.HONEYCOMB)
+					imageUri = nameCursor
+							.getString(nameCursor
+									.getColumnIndex(ContactsContract.CommonDataKinds.Phone.PHOTO_THUMBNAIL_URI));
+				else
+					imageUri = null;
 
-			String phoneNumber;
-			phoneNumber = "";
-			final Cursor phoneCursor = contentResolver
-					.query(ContactsContract.Data.CONTENT_URI, new String[] { Phone.NUMBER,
-							Phone.TYPE, Phone.LABEL }, ContactsContract.Data.CONTACT_ID + "=?"
-							+ " AND " + ContactsContract.Data.MIMETYPE + "='"
-							+ Phone.CONTENT_ITEM_TYPE + "'", new String[] { String.valueOf(id) },
-							null);
-			if (phoneCursor.moveToFirst()) {
-				final int phoneColumn = phoneCursor.getColumnIndex("data1");
-				phoneNumber = phoneCursor.getString(phoneColumn);
+				String phoneNumber;
+				phoneNumber = "";
+				phoneCursor = contentResolver.query(ContactsContract.Data.CONTENT_URI,
+						new String[] { Phone.NUMBER, Phone.TYPE, Phone.LABEL },
+						ContactsContract.Data.CONTACT_ID + "=?" + " AND "
+								+ ContactsContract.Data.MIMETYPE + "='" + Phone.CONTENT_ITEM_TYPE
+								+ "'", new String[] { String.valueOf(id) }, null);
+				if (phoneCursor.moveToFirst()) {
+					final int phoneColumn = phoneCursor.getColumnIndex("data1");
+					phoneNumber = phoneCursor.getString(phoneColumn);
+				}
+
+				String email = "";
+
+				emailCursor = contentResolver.query(
+						ContactsContract.CommonDataKinds.Email.CONTENT_URI, null,
+						ContactsContract.CommonDataKinds.Email.CONTACT_ID + " = ?",
+						new String[] { idStr }, null);
+				while (emailCursor.moveToNext()) {
+					email = emailCursor.getString(emailCursor
+							.getColumnIndex(ContactsContract.CommonDataKinds.Email.DATA));
+					if (email == null)
+						email = "";
+				}
+				emailCursor.close();
+
+				// this is ugly, but android is uglier
+				if (!((name == null || name.equals(""))))
+					aList.add(new ContactInfo(name, id, imageUri, phoneNumber, email));
+
 			}
 
-			String email = "";
+			return aList;
+		} finally {
+			if (nameCursor != null)
+				nameCursor.close();
 
-			final Cursor emailCursor = contentResolver.query(
-					ContactsContract.CommonDataKinds.Email.CONTENT_URI, null,
-					ContactsContract.CommonDataKinds.Email.CONTACT_ID + " = ?",
-					new String[] { idStr }, null);
-			while (emailCursor.moveToNext()) {
-				email = emailCursor.getString(emailCursor
-						.getColumnIndex(ContactsContract.CommonDataKinds.Email.DATA));
-				if (email == null)
-					email = "";
-			}
-			emailCursor.close();
+			if (phoneCursor != null)
+				phoneCursor.close();
 
-			// this is ugly, but android is uglier
-			if (!((name == null || name.equals(""))))
-				aList.add(new ContactInfo(name, id, imageUri, phoneNumber, email));
-
+			if (emailCursor != null)
+				emailCursor.close();
 		}
-
-		nameCur.close();
-		return aList;
 	}
 
 	public static Uri contactIdToTumbnailPhoto(final long id, final ContentResolver contentResolver) {
-		Uri $ = null;
+		final Uri $ = null;
 
-		if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.HONEYCOMB) {
+		if (android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.HONEYCOMB)
+			return null;
 
+		Cursor nameCursor = null;
+		try {
 			final List<String> projection = new ArrayList<String>();
 			projection.add(ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME);
 			projection.add(ContactsContract.CommonDataKinds.StructuredName.CONTACT_ID);
 			projection.add(ContactsContract.CommonDataKinds.Phone.PHOTO_THUMBNAIL_URI);
 
-			final Cursor nameCur = contentResolver.query(
+			nameCursor = contentResolver.query(
 					ContactsContract.Data.CONTENT_URI,
 					projection.toArray(new String[projection.size()]),
 					ContactsContract.Data.MIMETYPE + " = ? AND "
@@ -101,18 +119,16 @@ public class ContactsUtils {
 							Long.toString(id) },
 					ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME);
 
-			while (nameCur.moveToNext()) {
-				final String uriString = nameCur
-						.getString(nameCur
-								.getColumnIndex(ContactsContract.CommonDataKinds.Phone.PHOTO_THUMBNAIL_URI));
-				if (uriString != null)
-					$ = Uri.parse(uriString);
-				else
-					$ = null;
+			if (!nameCursor.moveToNext())
+				return null;
 
-			}
+			final String uriString = nameCursor.getString(nameCursor
+					.getColumnIndex(ContactsContract.CommonDataKinds.Phone.PHOTO_THUMBNAIL_URI));
+
+			return (uriString != null) ? Uri.parse(uriString) : null;
+		} finally {
+			if (nameCursor != null)
+				nameCursor.close();
 		}
-		return $;
 	}
-
 }
