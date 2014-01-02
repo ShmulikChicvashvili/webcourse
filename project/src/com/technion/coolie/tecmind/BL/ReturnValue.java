134cfce28279e157cf62708bc1c654a61cb8bf03
Shiran Yudilevich <shiran.afl@gmail.com>
2014-01-02 21:43:39 +0200

    ReturnValue.java

---
+++ project/src/com/technion/coolie/tecmind/BL/ReturnValue.java
@@ -0,0 +1,19 @@
+package com.technion.coolie.tecmind.BL;
+
+public enum ReturnValue {
+		 SUCCESS_TO_WRITE("SUCCESS_TO_WRITE"), SUCCESS_TO_READ("SUCCESS_TO_READ"), FAIL_TO_WRITE(
+		      "FAIL_TO_WRITE"), FAIL_TO_READ("FAIL_TO_READ"), SUCCESS_FROM_SERVER(
+		      "SUCCESS_FROM_SERVER"), SUCCESS_TO_SERVER("SUCCESS_TO_SERVER"), FAIL_FROM_SERVER(
+				      "FAIL_FROM_SERVER"), FAIL_TO_SERVER("FAIL_TO_SERVER"), ADD_USER_TO_SERVER("ADD_USER_TO_SERVER");
+
+		  private final String value;
+
+		  private ReturnValue(String s) {
+		    value = s;
+		  }
+
+		  public String value() {
+		    return value;
+		  }
+		  
+}
