dbc6e391759c5d658f797f31451390a20a3160d7
Eran Shapir <commandocamel@gmail.com>
2014-01-11 21:37:00 +0200

    added some features in the transactions

--- project/src/com/technion/coolie/techtrade/TransactionsActivity.java
+++ project/src/com/technion/coolie/techtrade/TransactionsActivity.java
@@ -1,36 +1,128 @@
 package com.technion.coolie.techtrade;
 
+import java.util.List;
 import java.util.Vector;
 
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
+import com.technion.coolie.CooliePriority;
 import com.technion.coolie.R;
+import com.technion.coolie.skeleton.CoolieAsyncRequest;
 
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class TransactionsActivity extends TechTradeActivity {
-	private ViewPager viewPager;
-	private TransactionsTabsPagerAdapter mAdapter;
+	private Vector<Product> mySalesVector;
+	private Vector<Product> myPurchasesVector;
+	private Vector<Product> myProductsVector;
+	private RelativeLayout rootView;
 	private LinearLayout tabStrip;
 	private Vector<View> tabs;
+	private ViewPager viewPager;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.get_transactions);
+		setContentView(R.layout.get_loading_screen);
+	
+		mySalesVector = new Vector<Product>();
+		myPurchasesVector = new Vector<Product>();
+		myProductsVector = new Vector<Product>();
 		
-		mAdapter = new TransactionsTabsPagerAdapter(getSupportFragmentManager());
-		viewPager = (ViewPager) findViewById(R.id.get_transactions_pager);
-		tabStrip = (LinearLayout) findViewById(R.id.get_transactions_tabstrip);
+		rootView = (RelativeLayout) getLayoutInflater().inflate(R.layout.get_transactions, null);
+		tabStrip = (LinearLayout) rootView.findViewById(R.id.get_transactions_tabstrip);
 		tabs = new Vector<View>();
+		viewPager = (ViewPager) rootView.findViewById(R.id.get_transactions_pager);
+		
 		initializeTabs();
-		viewPager.setAdapter(mAdapter);
+	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		
+		if(handleConnectivity() == false){
+			return;
+		}
+		
+		loadProductVectorsFromInternetIntoFragmentsAndDismissLoadingScreen();
+	}
+	
+	private void loadProductVectorsFromInternetIntoFragmentsAndDismissLoadingScreen() {
+		new CoolieAsyncRequest(getApplicationContext(), CooliePriority.IMMEDIATELY) {
+			
+			@Override
+			public Void actionOnServer(Void... params) {
+				TechTradeServer ttServer = new TechTradeServer();
+
+				fillMySalesVector();
+				fillMyPurchasesVector();
+				fillMyProductsVector();
+
+				return null;
+			}
+			
+			@Override
+			public Void onResult(Void result) {
+				initializeViewPager();
+				setContentView(rootView);
+				return null;
+			}
+
+			private void fillMyProductsVector() {
+				Product myProductsProduct = new Product(null, UserOperations.getUserId(), null, null, null, null, (byte[])null, null);
+				//List<Product> myProductsList = (List<Product>) ttServer.getPublishedProductsBySellerID(products[0]);
+				List<Product> myProductsList = Utils.getProductVectorStub(getApplicationContext(), 20);
+				if(myProductsList == null){
+					myProductsList = new Vector<Product>();
+				}
+				for (Product product : myProductsList) {
+					myProductsVector.add(product);
+				}
+			}
+
+			private void fillMyPurchasesVector() {
+				Product myPurchasesProduct = new Product(null, null, null, null, null, null, (byte[])null, null);
+				myPurchasesProduct.setBuyerId(UserOperations.getUserId());
+				//List<Product> myPurchasesList = (List<Product>) ttServer.getPurchasedProductsByBuyerID(products[0]);
+				List<Product> myPurchasesList = Utils.getProductVectorStub(getApplicationContext(), 10);
+				if(myPurchasesList == null){
+					myPurchasesList = new Vector<Product>();
+				}
+				for (Product product : myPurchasesList) {
+					myPurchasesVector.add(product);
+				}
+			}
+
+			private void fillMySalesVector() {
+				Product mySalesProduct = new Product(null, UserOperations.getUserId(), null, null, null, null, (byte[])null, null);
+				//List<Product> mySalesList = (List<Product>) ttServer.getSoldProductsBySellerID(products[0]);
+				List<Product> mySalesList = Utils.getProductVectorStub(getApplicationContext(), 0);
+				if(mySalesList == null){
+					mySalesList = new Vector<Product>();
+				}
+				for (Product product : mySalesList) {
+					mySalesVector.add(product);
+				}
+			}
+		}.run(); 
+	}
+
+	private void initializeViewPager() {
+		TransactionsTabsPagerAdapter transactionsFragmentsAdapter = 
+				new TransactionsTabsPagerAdapter(getSupportFragmentManager(), 
+													mySalesVector, myPurchasesVector, myProductsVector);
+		viewPager.setAdapter(transactionsFragmentsAdapter);
 		viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
 
 			@Override
@@ -48,7 +140,23 @@ public class TransactionsActivity extends TechTradeActivity {
 		});
 	}
 
+	private boolean handleConnectivity() {
+		if(NetworkOperations.isNetworkAvailable(getApplicationContext()) == false) {
+			Toast.makeText(getApplicationContext(), R.string.get_connection_error, Toast.LENGTH_LONG).show();
+			return false;
+		}
+		if(UserOperations.isUserConnected() == false){
+			//TODO open connection dialog
+			findViewById(R.id.get_loading_pbar).setVisibility(View.GONE);
+			((TextView) findViewById(R.id.get_loading_text)).setText(R.string.get_not_logged_in);
+			return false;
+		}
+		return true;
+	}
+
 	private void initializeTabs() {
+		tabs = new Vector<View>();
+		
 		OnClickListener listener = new OnClickListener() {
 
 			@Override
@@ -82,12 +190,6 @@ public class TransactionsActivity extends TechTradeActivity {
 			v.setBackgroundColor(inactiveColor);
 		}
 		newActiveTab.setBackgroundColor(activeColor);
-	}
-	
-	@Override
-	protected void onResume() {
-		super.onResume();
-		// TODO Auto-generated method stub
 	}
 	
 	@Override
